89799c360d8431a245df47466b7c35d54cbc748f
GKNSB <manoshatzis@gmail.com>
2021-02-27 14:06:52 +0200

    Well, had to start somewhere

---
+++ visionstudio-parser.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from time import sleep
+from playsound import playsound
+
+while(True):
+
+	checkNextPage = True
+	pageToCheck = 1
+	itemsFound = []
+
+	while(checkNextPage):
+
+		response = requests.get("https://www.visionstudio.gr/prodlist.asp?cat=5&sub=undefined&pmanuf=undefined&sort=undefined&step={0}&showfoto=0".format(pageToCheck))
+		soup = BeautifulSoup(response.text, 'html.parser')
+
+		table = soup.find("table", id="table18")
+		trs = table.find_all("tr")
+
+		for tr in trs:
+			if "3060" in tr.text:
+				
+				for img in tr.find_all("img"):
+					if "/544.jpg" in img["src"] or "/543.jpg" in img["src"] or "/542.jpg" in img["src"] or "/541.jpg" in img["src"]:
+						
+						itemsFound.append(tr.a.get("href"))
+
+		if len(trs) < 50:
+			checkNextPage = False
+		pageToCheck = pageToCheck + 1
+
+	for item in itemsFound:
+		print("https://www.visionstudio.gr{0}".format(item))
+		playsound("alarm.mp3")
+
+	sleep(10)
