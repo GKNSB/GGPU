46a92403cf17693ae97711618e55744e56e6e878
GKNSB <manoshatzis@gmail.com>
2021-04-09 21:46:13 +0300

    Slight fix

--- ggpu.py
+++ ggpu.py
@@ -6,6 +6,7 @@ import configparser
 import datetime
 import json
 
+
 config = configparser.RawConfigParser()
 config.read("configuration.config")
 webhookUrl = config.get("Config", "webhookUrl")
@@ -13,18 +14,18 @@ items = config.get("Config", "items")
 itemsDict = json.loads(items)
 
 
-def notify(results):
+def notify(results, searchItem):
 	if len(results) > 0:
 		for result in results:
-			print("{0} - Found one! > {1}".format(now.strftime("%d-%m-%Y %H:%M:%S"), result))
-			#webhook = DiscordWebhook(url=webhookUrl, content="@everyone found one! > {0}".format(result))
-			#response = webhook.execute()
+			print("{0} - Found a {1}! > {2}".format(now.strftime("%d-%m-%Y %H:%M:%S"), searchItem, result))
+			webhook = DiscordWebhook(url=webhookUrl, content="@everyone found a {0}! > {1}".format(searchItem, result))
+			response = webhook.execute()
 
 
 for item in list(itemsDict.keys()):
 	now = datetime.datetime.now()
 	print("{0} - Searching for {1} price {2}".format(now.strftime("%d-%m-%Y %H:%M:%S"), item, itemsDict[item]))
 	
-	notify(msystems(item, itemsDict[item]))
-	notify(visionstudio(item, itemsDict[item]))
-	notify(skroutz(item, itemsDict[item]))
+	notify(msystems(item, itemsDict[item]), item)
+	notify(visionstudio(item, itemsDict[item]), item)
+	notify(skroutz(item, itemsDict[item]), item)
